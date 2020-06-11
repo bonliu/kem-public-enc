1b9f51c5026912c2b887295766ccf20e8e10371b
bonliu <cheukbong1210@gmail.com>
2020-06-11 14:22:36 -0400

    Pass KEM file test

--- kem-enc.c
+++ kem-enc.c
@@ -57,6 +57,40 @@ int kem_encrypt(const char* fnOut, const char* fnIn, RSA_KEY* K)
 	/* TODO: encapsulate random symmetric key (SK) using RSA and SHA256;
 	 * encrypt fnIn with SK; concatenate encapsulation and cihpertext;
 	 * write to fnOut. */
+	
+	/* Generate X */
+	setSeed(0, 0);
+	size_t xLen = rsa_numBytesN(K);
+	unsigned char *X = malloc(xLen); X[xLen - 1] = 0;
+	randBytes(X, xLen - 1);	// Avoid reduction mod n
+
+	/* Open output file */
+	FILE *fp = fopen(fnOut, "wb");
+	if (fp == NULL) {
+		perror("[KEM-ENC] Output file");
+		exit(EXIT_FAILURE);
+	}
+
+	/* (RSA) Encrypt X */
+	unsigned char *xEnc = malloc(xLen);
+	rsa_encrypt(xEnc, X, xLen, K);
+	fwrite(xEnc, 1, xLen, fp);
+
+	/* H := SHA256 */
+	unsigned char H[HASHLEN];
+	SHA256(X, xLen, H);
+	fwrite(H, 1, HASHLEN, fp);
+
+	fclose(fp);
+	/* (SKE) Encrypt fnIn */
+	size_t offset = rsa_numBytesN(K) + HASHLEN;
+	SKE_KEY SK;
+	ske_keyGen(&SK, X, xLen);
+	ske_encrypt_file(fnOut, fnIn, &SK, NULL, offset);
+
+	free(X);
+	free(xEnc);
+	
 	return 0;
 }
 
@@ -65,8 +99,37 @@ int kem_decrypt(const char* fnOut, const char* fnIn, RSA_KEY* K)
 {
 	/* TODO: write this. */
 	/* step 1: recover the symmetric key */
+	size_t xLen = rsa_numBytesN(K);
+	unsigned char *X    = malloc(xLen);
+	unsigned char *xEnc = malloc(xLen);
+	FILE *fp = fopen(fnIn, "rb");
+	if (fp == NULL) {
+		perror("[KEM-DEC] Input file");
+		exit(EXIT_FAILURE);
+	}
+	fread(xEnc, 1, xLen, fp);
+	rsa_decrypt(X, xEnc, xLen, K);
+	
 	/* step 2: check decapsulation */
+	unsigned char   H[HASHLEN];
+	unsigned char mac[HASHLEN];
+	SHA256(X, xLen, H);
+	fread(mac, 1, HASHLEN, fp);
+	if (memcmp(H, mac, HASHLEN) != 0) {
+		fprintf(stderr, "WRONG MAC\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fclose(fp);
 	/* step 3: derive key from ephemKey and decrypt data. */
+	size_t offset = xLen + HASHLEN;
+	SKE_KEY SK;
+	ske_keyGen(&SK, X, xLen);
+	ske_decrypt_file(fnOut, fnIn, &SK, offset);
+
+	free(X);
+	free(xEnc);
+	
 	return 0;
 }
 
@@ -137,10 +200,65 @@ int main(int argc, char *argv[]) {
 	/* TODO: finish this off.  Be sure to erase sensitive data
 	 * like private keys when you're done with them (see the
 	 * rsa_shredKey function). */
+	RSA_KEY K;
+	FILE *rsaPub, *rsaPvt;
 	switch (mode) {
 		case ENC:
+			/* Read Public key from file */
+			rsaPub = fopen(fnKey, "rb");
+			if (rsaPub == NULL) {
+				perror("[ENC] Public key");
+				exit(EXIT_FAILURE);
+			}
+			rsa_readPublic(rsaPub, &K);
+			fclose(rsaPub);
+			
+			/* Encrypt */
+			kem_encrypt(fnOut, fnIn, &K);
+
+			rsa_shredKey(&K);
+			break;
 		case DEC:
+			/* Read Private key from file */
+			rsaPvt = fopen(fnKey, "rb");
+			if (rsaPvt == NULL) {
+				perror("[DEC] Private key");
+				exit(EXIT_FAILURE);
+			}
+			rsa_readPrivate(rsaPvt, &K);
+			fclose(rsaPvt);
+
+			/* Decrypt */
+			kem_decrypt(fnOut, fnIn, &K);
+
+			rsa_shredKey(&K);
+			break;
 		case GEN:
+			/* ./kem-enc -b 2048 -g /tmp/testkey */
+			/* Generate RSA key */
+			rsa_keyGen(nBits, &K);
+
+			/* Private key */
+			rsaPvt = fopen(fnOut, "wb");
+			if (rsaPvt == NULL) {
+				perror("[GEN] Private key");
+				exit(EXIT_FAILURE);
+			}
+			rsa_writePrivate(rsaPvt, &K);
+			fclose(rsaPvt);
+			
+			/* Public key */
+			strcat(fnOut, ".pub");
+			rsaPub = fopen(fnOut, "wb");
+			if (rsaPub == NULL) {
+				perror("[GEN] Public key");
+				exit(EXIT_FAILURE);
+			}
+			rsa_writePublic(rsaPub, &K);
+			fclose(rsaPub);
+			
+			rsa_shredKey(&K);
+			break;
 		default:
 			return 1;
 	}
--- ske.c
+++ ske.c
@@ -114,8 +114,56 @@ size_t ske_encrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
 size_t ske_encrypt_file(const char* fnout, const char* fnin,
 		SKE_KEY* K, unsigned char* IV, size_t offset_out)
 {
+	/* NOTE: offset determines where to begin writing to the output file.
+ * set to 0 to erase the file and write it from scratch. */
+
 	/* TODO: write this.  Hint: mmap. */
-	return 0;
+
+	/* Open input file */
+	int fdin = open(fnin, O_RDONLY);
+	struct stat sb;
+	if (fstat(fdin, &sb) == -1) {
+		perror("[SKE-ENC] Open input file");
+		exit(EXIT_FAILURE);
+	}
+	size_t msgLen = sb.st_size;
+
+	/* Map input file to memory */
+	unsigned char *inmap = mmap(NULL, msgLen, PROT_READ, MAP_PRIVATE, fdin, 0);
+	if (inmap == MAP_FAILED) {
+		perror("[SKE-ENC] Map input file");
+		exit(EXIT_FAILURE);
+	}
+	close(fdin);
+	
+	/* Open output file */
+	int fdout = open(fnout, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	if (fstat(fdout, &sb) == -1) {
+		perror("[SKE-ENC] Open output file");
+		exit(EXIT_FAILURE);
+	}
+	msgLen += 1; // include null char
+	size_t ctLen = ske_getOutputLen(msgLen);// + offset_out;
+	write(fdout, "a", ctLen);
+	
+	/* Map output file to memory */
+	unsigned char *outmap = mmap(NULL, ctLen + offset_out, 
+								 PROT_WRITE, MAP_SHARED, fdout, 0);
+	if (outmap == MAP_FAILED) {
+		perror("[SKE-ENC] Map output file");
+		exit(EXIT_FAILURE);
+	}
+	close(fdout);
+
+	/* Encrypt */
+	size_t bytesWritten = ske_encrypt(outmap + offset_out,
+									  inmap, msgLen, K, IV);
+
+	/* Clean up */
+	munmap(inmap, msgLen - 1);
+	munmap(outmap, ctLen + offset_out);	
+
+	return bytesWritten;
 }
 size_t ske_decrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
 		SKE_KEY* K)
@@ -165,6 +213,51 @@ size_t ske_decrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
 size_t ske_decrypt_file(const char* fnout, const char* fnin,
 		SKE_KEY* K, size_t offset_in)
 {
+	/* NOTE: offset determines where to begin reading the input file. */
 	/* TODO: write this. */
+
+	/* Open input file */
+	int fdin = open(fnin, O_RDONLY);
+	struct stat sb;
+	if (fstat(fdin, &sb) == -1) {
+		perror("[SKE-DEC] Open input file");
+		exit(EXIT_FAILURE);
+	}
+	size_t ctLen = sb.st_size;
+
+	/* Map input file to memory */
+	unsigned char *inmap = mmap(NULL, ctLen, PROT_READ, MAP_PRIVATE, fdin, 0);
+	if (inmap == MAP_FAILED) {
+		perror("[SKE-DEC] Map input file");
+		exit(EXIT_FAILURE);
+	}
+	close(fdin);
+	
+	/* Open output file */
+	int fdout = open(fnout, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	if (fstat(fdout, &sb) == -1) {
+		perror("[DKE-DEC] Open output file");
+		exit(EXIT_FAILURE);
+	}
+	/* Exclude null char */
+	size_t msgLen = ctLen - offset_in - AES_BLOCK_SIZE - HM_LEN - 1;
+	write(fdout, "a", msgLen);
+
+	/* Map output file to memory */
+	unsigned char *outmap = mmap(NULL, msgLen, PROT_WRITE, MAP_SHARED, fdout, 0);
+	if (outmap == MAP_FAILED) {
+		perror("[SKE-DEC] Map output file");
+		exit(EXIT_FAILURE);
+	}
+	close(fdout);
+
+	/* Decrypt */
+	// ske_decrypt((unsigned char*)pt,ct,ctLen,K);
+	ske_decrypt(outmap, inmap + offset_in, ctLen - offset_in, K);
+
+	/* Clean up */
+	munmap(inmap, ctLen);
+	munmap(outmap, msgLen);
+	
 	return 0;
 }
